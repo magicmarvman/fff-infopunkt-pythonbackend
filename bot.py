1c84eef15df3222426603d6ba2a197b4b2c2c9d6
Pascal Böhler <pascal.boehler@hotmail.de>
2019-09-28 21:27:18 +0200

    Changed server

--- bot.py
+++ bot.py
@@ -24,6 +24,7 @@ while True:
     messages = dict()
 
     groupIds = [update['message']['chat']['id'] for update in updates if 'text' in update['message']]
+    groupNames = [groupName['message']['chat']['title'] for groupName in updates if 'text' in groupName['message']]
 
     for groupId in groupIds:
         messages[str(groupId)] = [update['message']['text'] for update in updates if 'text' in update['message'] and groupId == update['message']['chat']['id']]
@@ -51,23 +52,42 @@ while True:
 
             try:
                 jsonFeed = message.split('\n')
+                #print(jsonFeed)
+                #print(len(jsonFeed))
+                if len(jsonFeed) == 9:
+                    print("EXEC 1")
+                    if jsonFeed[0].lower().strip() == 'none': # Title
+                        jsonFeed[0] = ""
+                    print("EXEC 2")
+                    if jsonFeed[1].lower().strip() == 'none': # Description
+                        jsonFeed[1] == ''
+                    if jsonFeed[8].lower().strip() == 'None': # organisation
+                        jsonFeed[8] = ''
+                    print("EXEC 3")
+                    if jsonFeed[2].lower().strip() == 'None': # Date
+                        jsonFeed[2] = ''
+                    print("EXEC 4")
+                    if jsonFeed[3].lower().strip() == 'None': # Time
+                        jsonFeed[3] = ''
+                    print("EXEC 5")
+                    if jsonFeed[4].lower().strip() == 'None': # meetingPoint / StartPoint
+                        jsonFeed[4] = ''
+                    print("EXEC 6")
+                    if jsonFeed[5].lower().strip() == 'None': # endPoint
+                        jsonFeed[5] = ''
+                    print("EXEC 7")
+                    if jsonFeed[6].lower().strip() == 'None': # routing
+                        jsonFeed[6] = ''
+                    print("EXEC 8")
+                    if jsonFeed[7].lower().strip() == 'None': # url
+                        jsonFeed[7] = ''
+                    print("EXEC 9")
+                    #jsonFeed.append(bot.getChat(groupIds)['title'])
+                    print(jsonFeed)
+                    datasets.append(jsonFeed)
 
-                if jsonFeed[0] == 'None':
-                    jsonFeed[0] = ""
-                if jsonFeed[1] == 'None':
-                    jsonFeed[1] == ''
-                if jsonFeed[2] == 'None':
-                    jsonFeed[2] = ''
-                if jsonFeed[3] == 'None':
-                    jsonFeed[3] = ''
-                if jsonFeed[4] == 'None':
-                    jsonFeed[4] = ''
-                if jsonFeed[5] == 'None':
-                    jsonFeed[5] = ''
-                if jsonFeed[6] == 'None':
-                    jsonFeed[6] = ''
-                datasets.append(jsonFeed)
             except:
+                #print("Error")
                 continue
 
     #print(datasets)
@@ -79,16 +99,20 @@ while True:
         model = Strike.StrikeModel()
         model.title = dataset[0]
         model.description = dataset[1]
-        model.datetime = dataset[2]
-        model.meetingPoint = dataset[3]
-        model.endPoint = dataset[4]
-        model.routeLength = dataset[5]
+        model.date = dataset[2]
+        model.startTime = dataset[3]
+        model.meetingPoint = dataset[4]
+        model.endPoint = dataset[5]
+        model.routeLength = dataset[6]
         model.latitude = 0
         model.longitude = 0
         model.strikeId = i
-        model.url = dataset[6]
+        model.url = dataset[7]
         model.searchTitle = dataset[0].lower()
         model.source = "Telegram chat group, collected bei FFF_Info Bot"
+        #model.groupSource = dataset[8]
+        model.groupSource = "FFF Info Test Group"
+        model.organisation = dataset[8]
 
         model.save()
         print("Stored!")
