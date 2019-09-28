e98cd53d9acaaeb73cf33756aa48a45fba0cdadd
Pascal Böhler <pascal.boehler@hotmail.de>
2019-09-28 18:07:36 +0200

    Changed bot.py and Strike.py to add AWS Support
    - finalized AWS Support
    - finalized Telegram Bot Support
    - added endless loop

--- Strike.py
+++ Strike.py
@@ -1,6 +1,8 @@
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute, NumberAttribute
+import json
 
+data = json.load(open('config.json'))
 
 class StrikeModel(Model):
     """
@@ -9,11 +11,23 @@ class StrikeModel(Model):
 
     class Meta:
         table_name = "FFF-Strikes"
+        # Specifies the region
+        region = 'eu-central-1'
+        # Access keys
+        aws_access_key_id = data['aws']['access_key']
+        aws_secret_access_key = data['aws']['private_key']
 
+
+    # The model:
     strikeId = NumberAttribute(hash_key=True)
-    datetime = NumberAttribute(range_key=True)
+    datetime = UnicodeAttribute()
     title = UnicodeAttribute()
     latitude = NumberAttribute()
     longitude = NumberAttribute()
     url = UnicodeAttribute()
     description = UnicodeAttribute()
+    meetingPoint = UnicodeAttribute()
+    endPoint = UnicodeAttribute()
+    routeLength = UnicodeAttribute()
+    searchTitle = UnicodeAttribute()
+    source = UnicodeAttribute()
--- bot.py
+++ bot.py
@@ -2,68 +2,76 @@ import telepot
 from telepot.delegate import per_chat_id, create_open, pave_event_space, include_callback_query_chat_id
 import jsonFormatter
 import json
+import Strike
 
+while True:
+    # load config files
+    with open('config.json') as json_data_file:
+        data = json.load(json_data_file)
 
+    telegram_bot_token = data['bot']['private_key']
 
-# load config files
-with open('config.json') as json_data_file:
-    data = json.load(json_data_file)
+    bot = telepot.Bot(telegram_bot_token)
 
-telegram_bot_token = data['bot']['private_key']
+    updates = bot.getUpdates()
+    print(updates)
+    messages = dict()
 
-bot = telepot.Bot(telegram_bot_token)
+    groupIds = [update['message']['chat']['id'] for update in updates if 'text' in update['message']]
 
-# get the data from server
-updates = bot.getUpdates()
-print(updates)
-# Filter the messages
-messages = dict()
+    for groupId in groupIds:
+        messages[str(groupId)] = [update['message']['text'] for update in updates if 'text' in update['message'] and groupId == update['message']['chat']['id']]
 
-groupIds = [update['message']['chat']['id'] for update in updates if 'text' in update['message']]
+    datasets = []
 
-for groupId in groupIds:
-    messages[str(groupId)] = [update['message']['text'] for update in updates if 'text' in update['message'] and groupId == update['message']['chat']['id']]
+    for messageSet in messages.values():
 
-#//print(messages)
-datasets = [[]]
+        title = ""
+        description = ""
+        dateTime = ""
+        startingPoint = ""
+        startingTime = "" # could be a time stamp
+        endPoint = ""
+        routeLength = ""
 
-#//print(messages)
+        jsonFeed = []
 
-for messageSet in messages.values():
+        for message in messageSet:
+            if (message == "help"):
+                continue
 
-    title = ""
-    description = ""
-    meetingPoint = ""
-    startingPoint = ""
-    startingTime = "" # could be a time stamp
-    endPoint = ""
-    routeLength = ""
+            try:
+                jsonFeed = message.split('\n')
 
-    jsonFeed = []
+                title = jsonFeed[0]
+                description = jsonFeed[1]
+                dateTime = jsonFeed[2]
+                startingPoint = jsonFeed[3]
+                endPoint = jsonFeed[4]
+                routeLength = jsonFeed[5]
+                website = jsonFeed[6]
+                datasets.append(jsonFeed)
+            except:
+                continue
 
-    for message in messageSet:
-        #print(message)
-        jsonFeed = message.split('\n')
+    print(datasets)
 
-        title = jsonFeed[0]
-        description = jsonFeed[1]
-        meetingPoint = jsonFeed[2]
-        startingPoint = jsonFeed[3]
-        endPoint = jsonFeed[4]
-        routeLength = jsonFeed[5]
-        print(jsonFeed)
-        datasets.append(jsonFeed)
+    i = 1
+    for dataset in datasets:
+        model = Strike.StrikeModel()
+        model.title = dataset[0]
+        model.description = dataset[1]
+        model.datetime = dataset[2]
+        model.meetingPoint = dataset[3]
+        model.endPoint = dataset[4]
+        model.routeLength = dataset[5]
+        model.latitude = 0
+        model.longitude = 0
+        model.strikeId = i
+        model.url = dataset[6]
+        model.searchTitle = dataset[0].lower()
+        model.source = "Telegram chat group, collected bei FFF_Info Bot"
 
-print(datasets[1])
+        model.save()
 
-
-
-
-
-
-
-'''
-datastock = json.dumps(message)
-
-print(datastock)
-'''
+        i += 1
