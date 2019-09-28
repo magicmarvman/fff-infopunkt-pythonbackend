3aca7090125e1d52b61961117b3af2af5917f8bb
Pascal Böhler <pascal.boehler@hotmail.de>
2019-09-28 16:35:43 +0200

    Fixed bugs + config file

--- bot.py
+++ bot.py
@@ -3,21 +3,64 @@ from telepot.delegate import per_chat_id, create_open, pave_event_space, include
 import jsonFormatter
 import json
 
-telegram_bot_token = "938606091:AAG8fQx3u7oCRup4jY_pQBvpIsRrliCt28A"
+# load config files
+with open('config.json') as json_data_file:
+    data = json.load(json_data_file)
+
+telegram_bot_token = data['bot']['private_key']
 
 bot = telepot.Bot(telegram_bot_token)
 
 # get the data from server
 updates = bot.getUpdates()
-
+print(updates)
 # Filter the messages
-message = {"": []}
+messages = dict()
 
 groupIds = [update['message']['chat']['id'] for update in updates if 'text' in update['message']]
 
 for groupId in groupIds:
-    message[String(groupId)] = messageText = [update['message']['text'] for update in updates if 'text' in update['message'] and groupId == update['message']['chat']['id']]
+    messages[str(groupId)] = [update['message']['text'] for update in updates if 'text' in update['message'] and groupId == update['message']['chat']['id']]
 
+#//print(messages)
+datasets = [[]]
+
+#//print(messages)
+
+for messageSet in messages.values():
+
+    title = ""
+    description = ""
+    meetingPoint = ""
+    startingPoint = ""
+    endPoint = ""
+    routeLength = ""
+
+    jsonFeed = []
+
+    for message in messageSet:
+        #print(message)
+        jsonFeed = message.split('\n')
+
+        title = jsonFeed[0]
+        description = jsonFeed[1]
+        meetingPoint = jsonFeed[2]
+        startingPoint = jsonFeed[3]
+        endPoint = jsonFeed[4]
+        routeLength = jsonFeed[5]
+        print(jsonFeed)
+        datasets.append(jsonFeed)
+
+print(datasets[1])
+
+
+
+
+
+
+
+'''
 datastock = json.dumps(message)
 
 print(datastock)
+'''
